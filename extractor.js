90cd845b72c30b4bf90f88c4769e875428ab700f
Tany CATALA BAILLY <catalatany@gmail.com>
2024-04-17 15:00:52 +0200

    bug de la librairie (surement) certaine image sont detecter mais d'autre non + ajout de different fichier de test

--- extractor.js
+++ extractor.js
@@ -7,15 +7,15 @@ const pdfSource = new Blob([fs.readFileSync(originalPdfPath)]);
 const fileType = "blob"; // or 'blob' based on your input type
 
 ExtractImages({ pdf: pdfSource, fileType: fileType }).then((images) => {
+  console.log(images);
   var imgName = 0;
   var filename;
   images.forEach(async (image) => {
     var imgType = image.imageType.split("/")[1];
-    console.log(image.url); // Blob URL for the image
+    console.log(image.imageType); // Blob URL for the image
     // You can use the blob URL to display the image or download it
     filename = await 'rendu/downloaded_image'+imgName+"."+imgType;
     imgName+=1;
-    console.log(filename);
     fs.writeFileSync(filename, Buffer.from(await image.blob.arrayBuffer()));
     console.log('Image downloaded successfully.');
     
