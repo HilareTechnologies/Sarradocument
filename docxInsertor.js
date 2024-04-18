06d295218722bf9c5631d725215119beb7b28203
Tany CATALA BAILLY <catalatany@gmail.com>
2024-04-18 16:54:55 +0200

    insertion d'image ne renvoie pas d'erreur mais ne fonctionne pas

--- docxInsertor.js
+++ docxInsertor.js
@@ -1,18 +1,22 @@
-const DocxImager = require('DocxImager');
+const {DocxImager} = require('docxImager');
+const fs = require('fs');
 
 let docxImager = new DocxImager();
 
 const [, , originalDocxPath] = process.argv;
 
-docxImager.load(originalDocxPath)
+async function Instertor(){
 
-const dir = 'rendu/word/media';
+    await docxImager.load(originalDocxPath);
+    const dir = 'rendu/word/media';
 
-fs.readdir(dir, (err, files) => {
-  console.log(files.length);
-   for(let i =0;i<files.length/2;i++){
-    docxImager.replaceWithImageURL('rendu/word/processed'+i+'.png', i, 'png');
-  }
-});
+    fs.readdir(dir, async (err, files) => {
+    console.log(files.length);
+    for(let i =1;i*2<=files.length/2;i++){
+        await docxImager.replaceWithLocalImage('rendu/word/processed/image'+i+'.png', i, 'png');
+    }
+    });
+    await docxImager.save('new_docx.docx');
+}
 
-docxImager.save('new_docx.docx');
+Instertor();
