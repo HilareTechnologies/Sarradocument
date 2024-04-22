e5b31237ccd13f9560e505f8378e56fbf7837744
Tany CATALA BAILLY <catalatany@gmail.com>
2024-04-22 14:05:48 +0200

    solution temporaire insertion et debut convertion

--- docxExtractor.js
+++ docxExtractor.js
@@ -2,7 +2,7 @@ const execSync = require('child_process').execSync;
 // import { execSync } from 'child_process';  // replace ^ if using ES modules
 
 const [, , originalDocxPath] = process.argv;
-const commande = 'docx-images extract --docx '+originalDocxPath+' --output-dir rendu --opts-module STUB.js';
+const commande = 'docx-images extract --docx '+originalDocxPath+' --output-dir ./ --opts-module STUB.js';
 
 const output = execSync(commande, { encoding: 'utf-8' });  // the default is 'buffer'
 console.log('Output was:\n', output); 
--- docxInsertor.js
+++ docxInsertor.js
@@ -5,18 +5,23 @@ let docxImager = new DocxImager();
 
 const [, , originalDocxPath] = process.argv;
 
-async function Instertor(){
+async function Insertor(){
 
     await docxImager.load(originalDocxPath);
-    const dir = 'rendu/word/media';
+    const dir = 'word/media';
 
     fs.readdir(dir, async (err, files) => {
-    console.log(files.length);
-    for(let i =1;i*2<=files.length/2;i++){
-        await docxImager.replaceWithLocalImage('rendu/word/processed/image'+i+'.png', i, 'png');
-    }
+        console.log(files.length);
+        for(let i =1;i*2<=files.length;i++){
+            
+            await docxImager.replaceWithLocalImage('word/media/image'+i+'.png', i, 'png');
+        }
     });
-    await docxImager.save('new_docx.docx');
-}
 
-Instertor();
+    
+    setTimeout(async () => {
+        await docxImager.save('new_docx.docx');
+    }, 100);
+}   
+
+Insertor();
