86991175bab71ac0d4998bb8d4937f70863fb84e
Tany CATALA BAILLY <catalatany@gmail.com>
2024-05-14 10:57:08 +0200

    convertor check alpha, ne converti que les png et suprime toute image inutile

--- convertor.js
+++ convertor.js
@@ -3,27 +3,52 @@ const Jimp = require('jimp');
 
 const [, , originalPdfPath] = process.argv;
 
+let fileRendu = 'rendu/converti'; 
+
+fs.readdir(fileRendu, async (err,files)=> {
+    for(file of files){
+        fs.unlink(fileRendu+"/"+file, (err) => {
+            if (err) {
+                // Handle specific error if any
+                if (err.code === 'ENOENT') {
+                    console.error('File does not exist.');
+                } else {
+                    throw err;
+                }
+            } else {
+                console.log('File deleted!');
+            }
+        });
+    }
+});
+
 fs.readdir(originalPdfPath,async (err, files) => {
     for(file of files){
         const image = await Jimp.read(originalPdfPath+"/"+file);
 
-        if (!image.hasAlpha()){
-            let name = file.split('.')[0];
-            // Save and overwrite the image
-            await image.writeAsync('rendu/converti/'+name+'.jpg');
+        let name = file.split('.')[0];
+        let extantion = file.split('.')[1];
+
+        if (!image.hasAlpha() && extantion=="png"){
+            
+            
+            await image.writeAsync(fileRendu+'/'+name+'.jpg');
+
+
         } else {
-            fs.unlink(originalPdfPath+"/"+file, (err) => {
-                if (err) {
-                    // Handle specific error if any
-                    if (err.code === 'ENOENT') {
-                        console.error('File does not exist.');
-                    } else {
-                        throw err;
-                    }
-                } else {
-                    console.log('File deleted!');
-                }
-            });
+            await image.writeAsync(fileRendu+'/'+name+'.'+extantion);
         }
+        fs.unlink(originalPdfPath+"/"+file, (err) => {
+            if (err) {
+                // Handle specific error if any
+                if (err.code === 'ENOENT') {
+                    console.error('File does not exist.');
+                } else {
+                    throw err;
+                }
+            } else {
+                console.log('File deleted!');
+            }
+        });
     };
 });
